d436dbeb977ebc193fe70e879631880c346f3d95
binsjoy <109491628+binsjoy@users.noreply.github.com>
2021-10-13 09:59:13 +0000

    Update readme [skip ci]

--- main.go
+++ main.go
@@ -13,4 +13,4 @@ func main() {
 	var x, y, z *gorgonia.Node
 	var err error
 	// define the expression
-	x = gorgonia.NewScalar(g, gorgonia.Float64, gor
+	x = gorgonia.NewScalar(g, gorgonia.Float64, gorg
