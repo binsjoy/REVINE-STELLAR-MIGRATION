5fbc00014ed969e3d5b96404cddb036d33f83546
binsjoy <109491628+binsjoy@users.noreply.github.com>
2021-11-18 09:35:20 +0000

    New translations template.pot (Ukrainian)

--- main.go
+++ main.go
@@ -14,4 +14,4 @@ func main() {
 	var err error
 	// define the expression
 	x = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("x"))
-	y = gorgonia.NewScalar(g, gorgonia.Flo
+	y = gorgonia.NewScalar(g, gorgonia.Float
