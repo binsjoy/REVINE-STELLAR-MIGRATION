ff26b1c092d6287408db69fdc22947c261b21950
binsjoy <109491628+binsjoy@users.noreply.github.com>
2021-11-28 22:28:38 +0000

    Bump.

--- main.go
+++ main.go
@@ -14,4 +14,4 @@ func main() {
 	var err error
 	// define the expression
 	x = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("x"))
-	y = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithNam
+	y = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("y
