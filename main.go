89094f0ee4538bf94c89d9a76bd01a5116c13270
binsjoy <109491628+binsjoy@users.noreply.github.com>
2023-03-26 11:46:16 +0000

    Version bump to 0.5.3

--- main.go
+++ main.go
@@ -33,4 +33,4 @@ func main() {
 	a := tensor.New(tensor.WithShape(2, 2), tensor.WithBacking([]int{1, 2, 3, 4}))
 	fmt.Printf("a: \n%v\n", a)
 	b := tensor.New(tensor.WithBacking(tensor.Range(tensor.Float32, 0, 24)), tensor.WithShape(2, 3, 4))
-	fmt.Printf("b: \n%v\n", b
+	fmt.Printf("b: \n%v\n", b)
